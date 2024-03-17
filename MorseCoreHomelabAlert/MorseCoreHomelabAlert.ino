4780341a8aa1aa2ae7760faf0f376f56511295d7
Roukurai <yo.soy.quien.soy.xd@gmail.com>
2024-03-16 18:54:32 -0600

    Initial commit

---
+++ MorseCoreHomelabAlert/MorseCoreHomelabAlert.ino
@@ -0,0 +1,341 @@
+
+// Declaring Variables
+String message = "HELLO WORLD";
+volatile bool PowerState = false;
+
+int charGap = 100;
+int letterGap = 200;
+int wordGap = 600;
+
+//Declaring Pins
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+int beepPin = 4;
+
+
+void setup() {
+  // ColorPin are configured for output
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  
+  // BuzzerPin is configured for output
+  pinMode(beepPin, OUTPUT);
+
+  // InputPin is configured for input
+  pinMode(2, INPUT);
+
+  //Start message loop Interrupt setup
+  attachInterrupt(digitalPinToInterrupt(2), TogglePowerState, RISING);
+  
+  //PowerStete Interrupt setup
+  attachInterrupt(digitalPinToInterrupt(3), PowerOff, RISING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (PowerState == true) {
+    int messageLenght = message.length();
+    for (int i = 0; i <= messageLenght && PowerState == true; i++) {
+      char testChar = message[i];
+      CheckChar(testChar);
+      delay(charGap);
+    }
+
+
+  } else {
+    SetColor(255, 0, 0);
+  }
+  delay(wordGap);
+}
+void TogglePowerState() {
+  PowerState = !PowerState;
+}
+
+void PowerOff(){
+  //comment
+  PowerState = false;
+  
+}
+
+void SetColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
+
+void dot() {
+  SetColor(0, 0, 255);
+  digitalWrite(beepPin, HIGH);
+  delay(100);
+  SetColor(0, 0, 0);
+  digitalWrite(beepPin, LOW);
+  delay(100);
+}
+
+void dash() {
+  SetColor(0, 255, 0);
+  digitalWrite(beepPin, HIGH);
+  delay(300);
+  SetColor(0, 0, 0);
+  digitalWrite(beepPin, LOW);
+  delay(100);
+}
+
+
+bool CheckChar(char CHAR) {
+  switch (CHAR) {
+    case 'A':
+      dot();
+      dash();
+      break;
+
+    case 'B':
+      dash();
+      for (int i = 0; i < 3; i++) {
+        dot();
+      };
+      break;
+
+    case 'C':
+      for (int i = 0; i < 2; i++) {
+        dash();
+        dot();
+      };
+      break;
+
+    case 'D':
+      dash();
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      break;
+
+    case 'E':
+      dot();
+      break;
+
+    case 'F':
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      dash();
+      dot();
+      break;
+
+    case 'G':
+      for (int i = 0; i < 2; i++) {
+        dash();
+      };
+      dot();
+      break;
+
+    case 'H':
+      for (int i = 0; i < 4; i++) {
+        dot();
+      };
+      break;
+
+    case 'I':
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      break;
+
+    case 'J':
+      dot();
+      for (int i = 0; i < 3; i++) {
+        dash();
+      };
+      break;
+
+    case 'K':
+      dash();
+      dot();
+      dash();
+      break;
+
+    case 'L':
+      dot();
+      dash();
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      break;
+
+    case 'M':
+      for (int i = 0; i < 3; i++) {
+        dash();
+      };
+      break;
+
+    case 'N':
+      dash();
+      dot();
+      break;
+
+    case 'O':
+      for (int i = 0; i < 3; i++) {
+        dash();
+      };
+      break;
+
+    case 'P':
+      for (int i = 0; i < 2; i++) {
+        dash();
+        dot();
+      };
+      break;
+
+    case 'Q':
+      dot();
+      dash();
+      dot();
+      break;
+
+    case 'R':
+      dot();
+      dash();
+      dot();
+      break;
+
+    case 'S':
+      for (int i = 0; i < 3; i++) {
+        dot();
+      };
+      break;
+
+    case 'T':
+      dash();
+      break;
+
+    case 'U':
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      dash();
+      break;
+
+    case 'V':
+      for (int i = 0; i < 3; i++) {
+        dot();
+      };
+      dash();
+      break;
+
+    case 'W':
+      dot();
+      for (int i = 0; i < 2; i++) {
+        dash();
+      };
+      break;
+
+    case 'X':
+      dash();
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      dash();
+      break;
+
+    case 'Y':
+      dash();
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      dash();
+      break;
+
+    case 'Z':
+      for (int i = 0; i < 2; i++) {
+        dash();
+      };
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      break;
+
+    case '1':
+      dot();
+      for (int i = 0; i < 4; i++) {
+        dash();
+      };
+      break;
+
+    case '2':
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      for (int i = 0; i < 3; i++) {
+        dash();
+      };
+      break;
+
+    case '3':
+      for (int i = 0; i < 3; i++) {
+        dot();
+      };
+      for (int i = 0; i < 2; i++) {
+        dash();
+      };
+      break;
+
+    case '4':
+      for (int i = 0; i < 4; i++) {
+        dot();
+      };
+      dash();
+      break;
+
+    case '5':
+      for (int i = 0; i < 5; i++) {
+        dot();
+      };
+      break;
+
+    case '6':
+      dash();
+      for (int i = 0; i < 4; i++) {
+        dot();
+      };
+      break;
+
+    case '7':
+      for (int i = 0; i < 2; i++) {
+        dash();
+      };
+      break;
+
+    case '8':
+      for (int i = 0; i < 3; i++) {
+        dash();
+      };
+      for (int i = 0; i < 2; i++) {
+        dot();
+      };
+      break;
+
+    case '9':
+      for (int i = 0; i < 4; i++) {
+        dash();
+      };
+      break;
+
+    case '0':
+      for (int i = 0; i < 5; i++) {
+        dash();
+      };
+      break;
+    case ' ':
+      delay(wordGap);
+      break;
+
+    default:
+      return false;
+      break;
+  }
+
+
+  return true;
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # morseCodeHomelabAlert
 Homelab alerts will be provided via morse code, visually and audible. Planting the base for AlertToSpeech
+
+Purpose:
+This Arduino code is designed to use an LED and a piezo buzzer to display a message in Morse code when requested by the homelab. The message is coded in Morse, and the LEDs light up according to the Morse code pattern while the buzzer produces corresponding sounds.
+
+Declarations:
+message: A string variable that stores the message to be displayed in Morse code.
+PowerState: A boolean variable that represents the state of the system (on/off).
+charGap, letterGap, wordGap: Integer variables representing the time gaps between characters, letters, and words in Morse code, respectively.
+redPin, greenPin, bluePin, beepPin: Integer variables representing the pins connected to the RGB LED and the buzzer.
+
+Functions:
+void setup(): The initialization function called once when the program starts. It sets the pinMode for the LED and buzzer pins and sets up interrupts for the button pins.
+
+void loop(): The main execution loop that repeatedly checks the PowerState and displays the message in Morse code if the PowerState is true.
+
+void TogglePowerState(): Interrupt service routine (ISR) that toggles the PowerState when the button is pressed.
+
+void PowerOff(): ISR that sets the PowerState to false, turning off the system.
+
+void SetColor(int redValue, int greenValue, int blueValue): Function to set the color of the RGB LED by writing PWM values to its pins.
+
+void dot(): Function to produce a dot in Morse code pattern by lighting up the LED and emitting a short beep.
+
+void dash(): Function to produce a dash in Morse code pattern by lighting up the LED and emitting a long beep.
+
+bool CheckChar(char CHAR): Function to check each character in the message and translate it to Morse code. It calls dot() and dash() functions accordingly.
+
+Morse Code Translation:
+The CheckChar() function translates each character in the message to Morse code and calls corresponding functions (dot() or dash()) to display the pattern. The Morse code for each character is defined within a switch-case statement.
+
+Usage:
+Connect the RGB LED and the piezo buzzer to the specified pins on the Arduino board.
+Upload the code to the Arduino.
+Press the button connected to pin 2 to toggle the display of the message in Morse code.
+Press the button connected to pin 3 to turn off the system.
+Notes:
+Adjust the time gaps (charGap, letterGap, wordGap) for proper Morse code timing.
+Modify the message variable to display a different message in Morse code.
+
+
+
+
+
